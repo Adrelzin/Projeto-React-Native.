11661aa7e8addb38f258e82095a1a86edb2ded1c
Adrelzin <adrelwilion@gmail.com>
2026-02-05 19:55:37 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Adryell William Gomes Nascimento</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#008000',
     alignItems: 'center',
     justifyContent: 'center',
   },
